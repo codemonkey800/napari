228a9a9ecd96e4da8a7634c78bf718ccc198ed07
codemonkey800 <codemonkey800@users.noreply.github.com>
2022-02-25 17:31:58 +0000

    deploy: 5847b411d796e6e98f4cfb4045fa4ab5c11c8c3f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ga2bb5c3',
+    VERSION: '0.1.dev1+g5847b41',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
